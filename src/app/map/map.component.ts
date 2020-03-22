17c826852e27c1e33b9ee448ff1ed9fdc4ad358f
Saurabh Verma <saurabhshalu@gmail.com>
2020-03-23 00:19:31 +0530

    interactive map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -17,7 +17,7 @@ export class MapComponent implements OnInit {
   public c_codes: any = null;
   public covidData: any = null;
   public colorlist: any = {}
-
+  
   public calculateEverything() {
     console.log(this.c_codes['INDIA'])
     let maxcase = this.covidData[0]['cases']
@@ -41,9 +41,34 @@ export class MapComponent implements OnInit {
     this.http.get('./assets/map_data/c_codes.json').subscribe((data: any) => {
         this.c_codes = data;
         //this.http.get('https://corona.lmao.ninja/countries').subscribe((data: any[]) => {
-        this.http.get('./assets/local_corona.json').subscribe((data: any[]) => {
-          this.covidData = data;
+        this.http.get('./assets/local_corona.json').subscribe((cdata: any[]) => {
+          this.covidData = cdata;
           $(document).ready(function() {
+            var caseslist = {};
+            var deathslist = {};
+            var recoveredlist = {};
+
+            for(var i=0;i<cdata.length;i++) {
+              var t1 = data[cdata[i]['country'].toUpperCase()]
+              if(t1 !==undefined) {
+                caseslist[t1] = cdata[i]['cases'];
+                deathslist[t1] = cdata[i]['deaths'];
+                recoveredlist[t1] = cdata[i]['recovered'];
+              }
+            }
+
+            // function getIndex(name, data) {
+            //   let x = 0;
+            //   for(var i=0;i<data.length;i++) {
+            //     if(data[i]['country'].toUpperCase()==name.toUpperCase()) {    
+            //       return x;
+            //     }
+            //     x++;
+            //   }
+            //   return -1;
+            // }
+            
+
             $('#vmap').vectorMap({
               map: 'world_en',
               backgroundColor: 'rgb(208, 208, 211)',
@@ -51,9 +76,35 @@ export class MapComponent implements OnInit {
               borderOpacity: 0.7,
               borderWidth: 1,
               color: '#62d2a2',
-              onLabelShow: function(event, label, code) {  
-                //label[0].innerHTML = label[0].innerHTML + " : - cases";
+              onLabelShow: function(event, label, code) {
                 label[0].style.fontSize = '18px';
+                var temp = caseslist[code.toUpperCase()];
+                if(temp!=undefined)
+                {   
+                  document.getElementById('floating-data').style.display = 'block';
+                  label[0].innerHTML = label[0].innerHTML + " : " + temp + " cases";
+                  document.getElementById('totcase').innerHTML = caseslist[code.toUpperCase()];
+                  document.getElementById('totrec').innerHTML = recoveredlist[code.toUpperCase()];
+                  document.getElementById('totdeath').innerHTML = deathslist[code.toUpperCase()];  
+                  var tx = label[0].innerHTML;
+                  document.getElementById('country_name').innerHTML = tx.substr(0,tx.indexOf(':'));
+                }
+                else{
+                  document.getElementById('floating-data').style.display = 'none';
+                }
+              },
+              onRegionClick: function(element, code, region)
+              {
+                if(caseslist[code.toUpperCase()]!=undefined) {
+                  document.getElementById('floating-data').style.display = 'block';
+                  document.getElementById('totcase').innerHTML = caseslist[code.toUpperCase()];
+                  document.getElementById('totrec').innerHTML = recoveredlist[code.toUpperCase()];
+                  document.getElementById('totdeath').innerHTML = deathslist[code.toUpperCase()];
+                  document.getElementById('country_name').innerHTML = region;
+                }
+                else {
+                  document.getElementById('floating-data').style.display = 'none';
+                }      
               }
             });
             this.calculateEverything();
@@ -75,20 +126,6 @@ export class MapComponent implements OnInit {
     });
   }
   ngAfterViewInit(): void {
-    // $(document).ready(function() {
-    //   $('#vmap').vectorMap({
-    //     map: 'world_en',
-    //     backgroundColor: 'transparent',
-    //     scaleColors: ['#b6d6ff', '#005ace'],
-    //     colors: this.mycolors
-    //   });
 
-    //     // $('#vmap').vectorMap('set', 'colors', { in: 'rgba(99,99,220,0.5)', be: '#b10', bg: '#b10'});
-        
-    //   (<any>document.getElementsByClassName('jqvmap-zoomin'))[0].style.height = "15" + "px";
-    //   (<any>document.getElementsByClassName('jqvmap-zoomin'))[0].style.width = "15" + "px";
-    //   (<any>document.getElementsByClassName('jqvmap-zoomout'))[0].style.height = "15" + "px";
-    //   (<any>document.getElementsByClassName('jqvmap-zoomout'))[0].style.width = "15" + "px";
-    // }.bind(this));
   }
 }
