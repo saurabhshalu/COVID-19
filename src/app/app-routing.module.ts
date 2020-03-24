4bb485f77bd844178efd99121cf449d5cf35c260
Saurabh Verma <saurabhshalu@gmail.com>
2020-03-24 12:25:02 +0530

    maintainance mode is on

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { WelcomeComponent } from './welcome/welcome.component';
-import { MapComponent } from './map/map.component';
-import { DetailsComponent } from './details/details.component';
-import { HelpComponent } from './help/help.component';
+import { MaintainanceComponent } from './maintainance/maintainance.component';
 
 
 const routes: Routes = [
   {
-    path: 'help',
-    component: HelpComponent
-  },
-  {
-    path: 'details',
-    component: DetailsComponent
-  },
-  {
-    path: 'map',
-    component: MapComponent
-  },
-  {
     path: '**',
-    component: WelcomeComponent
+    component: MaintainanceComponent
   }
 ];
 
